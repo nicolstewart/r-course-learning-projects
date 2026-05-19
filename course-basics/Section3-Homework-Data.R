f2b1645019b7e0333c32e544d4947d3807eb44b3
nicolstewart <nicolstewart.business@gmail.com>
2026-05-19 15:26:24 +0200

    Added basics R exercise

---
+++ course-basics/Section3-Homework-Data.R
@@ -0,0 +1,61 @@
+#Data
+revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
+expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
+
+#this is a simple financial analysis task, only vectors wanted 
+#Solution
+#calculating the proft for each month 
+
+profit <- revenue - expenses 
+profit
+
+#calculating the profit after tax for each month (with a tax rate of 30%)
+tax <- round(profit * 0.3, digits =2)
+tax
+
+profit.after.tax <- profit - tax
+profit.after.tax
+
+#calculating the profit margin for each month 
+profit.margin <- round(profit.after.tax / revenue, 2) * 100
+profit.margin
+
+#calculating the good months
+mean_pat <- mean(profit.after.tax)
+mean_pat
+
+good.months <- profit.after.tax > mean_pat
+good.months
+
+#calculating the bad months
+bad.months <- !good.months
+bad.months
+
+#finding the best month 
+best.month <- profit.after.tax == max(profit.after.tax)
+best.month
+
+#finding the worst month 
+worst.month <- profit.after.tax == min(profit.after.tax)
+worst.month
+
+#units of thousands
+revenue.1000 <- round(revenue / 1000)
+revenue.1000
+expenses.1000 <- round(expenses / 1000)
+profit.1000 <- round(profit / 1000)
+profit.after.tax.1000 <- round(profit.after.tax / 1000)
+
+#outputs - matrices used to make output look cleaner and easier to read 
+m <- rbind(
+  revenue.1000,
+  expenses.1000,
+  profit.1000,
+  profit.after.tax.1000,
+  profit.margin,
+  good.months,
+  bad.months,
+  best.month,
+  worst.month
+)
+m
